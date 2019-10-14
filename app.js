4c4a3f73818cafaca79497caa63af1c472a51005
takashiAg <crawd4274@gmail.com>
2019-10-14 16:18:30 +0900

    csv 形式

--- app.js
+++ app.js
@@ -13,13 +13,13 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 })
 var indexRouter = require('./routes/index');
 app.use('/', indexRouter);
@@ -31,10 +31,10 @@ var getDataRouter = require('./routes/getData');
 app.use('/getData', getDataRouter);
 
 var getCSVRouter = require('./routes/getCSV');
-app.use('/getCSV', getCSVRouter);
+app.use('/getData.csv', getCSVRouter);
 
 var getLinedCSVRouter = require('./routes/getLinedCSV');
-app.use('/getLinedCSV', getLinedCSVRouter);
+app.use('/getLined.csv', getLinedCSVRouter);
 
 var ImplicitGrantFlowRouter = require('./routes/ImplicitGrantFlow');
 app.use('/ImplicitGrantFlow', ImplicitGrantFlowRouter);
@@ -43,19 +43,19 @@ var usersRouter = require('./routes/users');
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
